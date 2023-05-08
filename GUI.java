33c4830a720cc4509471ee26a8391707ef916056
rhonegoodfellow <boomcopia8@gmail.com>
2023-05-08 09:03:59 -0700

    Added GUI class and setup the frame

---
+++ GUI.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+import java.awt.event.ActionListener;
+
+public class GUI{
+    public GUI(){
+    }
+
+    public int quadraticGUI(){
+        JFrame frame = new JFrame();
+        JPanel panel = new JPanel();
+        JLabel label = new JLabel();
+        JButton button = new JButton();
+        frame.setVisible(true);
+        return 0;
+    }
+}
--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
+
 public class Main {
     public static void main(String[] args) {
+        GUI myGUI = new GUI();
         System.out.println("Hello World!");
+        myGUI.quadraticGUI();
     }
 }
