99a3c9ae92a06f765d3a04d12a480900263a2004
rhonegoodfellow <boomcopia8@gmail.com>
2023-05-09 08:08:58 -0700

    Sized the frame and added a title

--- GUI.java
+++ GUI.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionListener;
 
 public class GUI{
@@ -6,11 +7,20 @@ public class GUI{
     }
 
     public int quadraticGUI(){
-        JFrame frame = new JFrame();
+        JFrame frame = new JFrame("Quadratic equationator");
         JPanel panel = new JPanel();
         JLabel label = new JLabel();
         JButton button = new JButton();
+        JFormattedTextField textField = new JFormattedTextField();
+        panel.setBorder(BorderFactory.createEmptyBorder(100,100,100,100));
+        panel.setLayout(new GridLayout(0,1));
+
+
         frame.setVisible(true);
+        frame.add(panel, BorderLayout.CENTER);
+        frame.pack();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
         return 0;
     }
 }
