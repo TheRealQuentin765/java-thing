68c81d3da93995def9391677bc3bb87fdf155781
rhonegoodfellow <boomcopia8@gmail.com>
2023-05-09 09:24:40 -0700

    Working frame

--- GUI.java
+++ GUI.java
@@ -3,19 +3,21 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class GUI implements ActionListener{
-    public GUI(){
+public class GUI {
+    public GUI() {
     }
 
-    public int quadraticGUI(){
+//    JFormattedTextField textField = new JFormattedTextField(2);
+
+    public int quadraticGUI() {
         JFrame frame = new JFrame("Quadratic equationator");
         JPanel panel = new JPanel();
         JLabel label = new JLabel();
         JButton button = new JButton();
-        JFormattedTextField textField = new JFormattedTextField(2);
-        textField.addActionListener(this);
-        panel.setBorder(BorderFactory.createEmptyBorder(100,100,100,100));
-        panel.setLayout(new GridLayout(0,1));
+
+//        textField.addActionListener(this);
+        panel.setBorder(BorderFactory.createEmptyBorder(100, 100, 100, 100));
+        panel.setLayout(new GridLayout(0, 1));
 
         frame.setVisible(true);
         frame.add(panel, BorderLayout.CENTER);
@@ -24,8 +26,10 @@ public class GUI implements ActionListener{
 
         return 0;
     }
-    public actionPerformed(ActionEvent event){
-//        text
-        //Change in branch_1
-    }
+
+//    public void actionPerformed(ActionEvent event) {
+//        String text = textField.getText();
+//        textField.selectAll();
+//    }
 }
+
