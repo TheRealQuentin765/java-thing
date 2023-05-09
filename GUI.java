322b97c27ce40598630a316648f9c9f84f26c9c3
rhonegoodfellow <boomcopia8@gmail.com>
2023-05-09 08:56:05 -0700

    Change in branch comment

--- GUI.java
+++ GUI.java
@@ -1,8 +1,9 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class GUI{
+public class GUI implements ActionListener{
     public GUI(){
     }
 
@@ -11,10 +12,10 @@ public class GUI{
         JPanel panel = new JPanel();
         JLabel label = new JLabel();
         JButton button = new JButton();
-        JFormattedTextField textField = new JFormattedTextField();
+        JFormattedTextField textField = new JFormattedTextField(2);
+        textField.addActionListener(this);
         panel.setBorder(BorderFactory.createEmptyBorder(100,100,100,100));
         panel.setLayout(new GridLayout(0,1));
-
 
         frame.setVisible(true);
         frame.add(panel, BorderLayout.CENTER);
@@ -22,5 +23,9 @@ public class GUI{
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         return 0;
+    }
+    public actionPerformed(ActionEvent event){
+//        text
+        //Change in branch_1
     }
 }
