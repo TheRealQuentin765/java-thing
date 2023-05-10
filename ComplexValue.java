7c42c10643ed7814bf2824753a90a4e29a71f503
Quentin <quentin@team-farrell.com>
2023-05-10 09:19:15 -0700

    Added in ComplexValue class which can hold complex numbers and do a bunch of math related operations on them, currently supports:  getting real, getting imaginary, adding, negative, subtracting, multiplying, inverse, dividing, radius squared, radius, argument, and power  also support checking equality with another Complex value and with a double  can be constructed from both the real and imaginary parts or just the real

---
+++ ComplexValue.java
@@ -0,0 +1,89 @@
+public class ComplexValue {
+    private final double real;
+    private final double imaginary;
+
+    public ComplexValue(double real) {
+        this(real,0);
+    }
+
+    public ComplexValue(double real, double imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+
+    public double getReal() {
+        return real;
+    }
+
+    public double getImaginary() {
+        return imaginary;
+    }
+
+    public boolean equals(double val) {
+        return imaginary == 0 && real == val;
+    }
+
+    public boolean equals(ComplexValue obj) {
+        return (real == obj.real && imaginary == obj.imaginary);
+    }
+
+    public ComplexValue add(ComplexValue obj) {
+        return new ComplexValue(real+obj.real, imaginary+ obj.imaginary);
+    }
+
+    public ComplexValue multiply(ComplexValue obj) {
+        return new ComplexValue(
+                real*obj.real - imaginary*obj.imaginary,
+                real* obj.imaginary + imaginary*obj.real
+        );
+    }
+
+    public ComplexValue negative() {
+        return new ComplexValue(-real, -imaginary);
+    }
+
+    public ComplexValue inverse() { // the formula comes from the internet
+        double d = Math.pow(real,2)+Math.pow(imaginary,2);
+        return new ComplexValue(real/d,-imaginary/d);
+    }
+
+    public ComplexValue divide(ComplexValue obj) {
+        return multiply(obj.inverse());
+    }
+
+    public ComplexValue subtract(ComplexValue obj) {
+        return add(obj.negative());
+    }
+
+    public double radius() {
+        return Math.sqrt(radiusSqr());
+    }
+
+    public double radiusSqr() {
+        return Math.pow(real,2)+Math.pow(imaginary,2);
+    }
+
+    public ComplexValue squareRoot() { // this formula also comes from the internet
+        double r = radius();
+        return new ComplexValue(
+                Math.sqrt((r+real)/2),
+                Math.signum(imaginary) * Math.sqrt((r-real)/2)
+        );
+    }
+
+    public double argument() {
+        return Math.atan2(imaginary,real);
+    }
+
+    public ComplexValue pow(ComplexValue obj) { // math from https://mathworld.wolfram.com/ComplexExponentiation.html
+        double r2 = radiusSqr();
+        double arg = argument();
+        double newR = Math.pow(r2,obj.real/2);
+        double newArg = obj.real*arg + obj.imaginary*Math.log(r2)/2;
+        return new ComplexValue(
+                newR*Math.cos(newArg),
+                newR*Math.sin(newArg)
+        );
+    }
+
+}
