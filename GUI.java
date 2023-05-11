7bb768c1a83a540fea0452f17f45f886aebfdbda
rhonegoodfellow <boomcopia8@gmail.com>
2023-05-11 09:07:46 -0700

    A little help from chat GPT to get the frame to work properly

--- GUI.java
+++ GUI.java
@@ -8,41 +8,41 @@ public class GUI implements ActionListener {
     JFormattedTextField textFieldB = new JFormattedTextField("b");
     JFormattedTextField textFieldC = new JFormattedTextField("c");
 
+    JLabel labelSquared = new JLabel("x^2");
+    JLabel labelX = new JLabel("x");
+
     JButton button = new JButton("Calculate");
     JPanel panel = new JPanel();
     JFrame frame = new JFrame("Quadratic equationator");
     JLabel label = new JLabel("Result: ");
     String textA;
+
     public GUI() {
+        panel.setLayout(null); // Use null layout
 
-        textFieldA.setColumns(10);
-        textFieldA.addActionListener(this);
-
-        label.setAlignmentX(30);
-        label.setAlignmentY(40);
-
-        panel.setBorder(BorderFactory.createEmptyBorder(100, 100, 100, 100));
-        panel.setLayout(null);
-        label.setBounds(100,200,100,100);
-        frame.setBounds(500,500,1000,1000);
-
+        textFieldA.setBounds(10, 10, 20, 20);
+        textFieldB.setBounds(50, 10, 20, 20);
+        textFieldC.setBounds(90, 10, 20, 20);
+        button.setBounds(10, 50, 100, 30);
+        label.setBounds(10, 140, 200, 20);
 
         panel.add(textFieldA);
+        panel.add(textFieldB);
+        panel.add(textFieldC);
         panel.add(button);
         panel.add(label);
 
-
         button.addActionListener(this);
 
-        frame.add(panel, BorderLayout.CENTER);
+        frame.setContentPane(panel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
+        frame.setBounds(100, 100, 300, 200); // Set frame size and position
         frame.setVisible(true);
     }
-
     public void actionPerformed(ActionEvent event) {
-       if(event.getSource() == button) {
+        if(event.getSource() == button) {
            textA = textFieldA.getText();
+           String textB = textFieldB.getText();
            textFieldA.selectAll();
            System.out.println(textA);
            label.setText("Result: " + textA);
