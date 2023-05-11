5ee78c1b4883c8e9a64e74b3ca0589c0acfcfcfc
rhonegoodfellow <boomcopia8@gmail.com>
2023-05-11 08:12:10 -0700

    Gets the text from the text box and sets it to a variable

--- GUI.java
+++ GUI.java
@@ -3,33 +3,44 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class GUI {
+public class GUI implements ActionListener {
+    JFormattedTextField textFieldA;
+    JButton button = new JButton("Calculate");
+    JPanel panel = new JPanel();
+    JFrame frame = new JFrame("Quadratic equationator");
+    JLabel label = new JLabel("Result: ");
+    String textA;
     public GUI() {
-    }
+        textFieldA = new JFormattedTextField("Wassup");
+        textFieldA.setColumns(10);
+        textFieldA.addActionListener(this);
 
-//    JFormattedTextField textField = new JFormattedTextField(2);
+        label.setAlignmentX(30);
+        label.setAlignmentY(40);
 
-    public int quadraticGUI() {
-        JFrame frame = new JFrame("Quadratic equationator");
-        JPanel panel = new JPanel();
-        JLabel label = new JLabel();
-        JButton button = new JButton();
-
-//        textField.addActionListener(this);
         panel.setBorder(BorderFactory.createEmptyBorder(100, 100, 100, 100));
         panel.setLayout(new GridLayout(0, 1));
 
-        frame.setVisible(true);
-        frame.add(panel, BorderLayout.CENTER);
-        frame.pack();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        panel.add(textFieldA);
+        panel.add(button);
+        panel.add(label);
 
-        return 0;
+
+        button.addActionListener(this);
+
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
     }
 
-//    public void actionPerformed(ActionEvent event) {
-//        String text = textField.getText();
-//        textField.selectAll();
-//    }
+    public void actionPerformed(ActionEvent event) {
+       if(event.getSource() == button) {
+           textA = textFieldA.getText();
+           textFieldA.selectAll();
+           System.out.println(textA);
+//           panel.add(new JLabel("Hello"));
+       }
+    }
 }
 
--- Main.java
+++ Main.java
@@ -3,6 +3,6 @@ public class Main {
     public static void main(String[] args) {
         GUI myGUI = new GUI();
         System.out.println("Hello World!");
-        myGUI.quadraticGUI();
+
     }
 }
