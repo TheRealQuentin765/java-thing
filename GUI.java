6cec627067be76e7ccb8fde649e49001f6e0fdd1
rhonegoodfellow <boomcopia8@gmail.com>
2023-05-11 09:23:31 -0700

    The gui is essentially done, made the textboxes and labels so they line up to get the a b and c values

--- GUI.java
+++ GUI.java
@@ -8,8 +8,8 @@ public class GUI implements ActionListener {
     JFormattedTextField textFieldB = new JFormattedTextField("b");
     JFormattedTextField textFieldC = new JFormattedTextField("c");
 
-    JLabel labelSquared = new JLabel("x^2");
-    JLabel labelX = new JLabel("x");
+    JLabel labelSquared = new JLabel("x^2 +");
+    JLabel labelX = new JLabel("x +");
 
     JButton button = new JButton("Calculate");
     JPanel panel = new JPanel();
@@ -20,12 +20,16 @@ public class GUI implements ActionListener {
     public GUI() {
         panel.setLayout(null); // Use null layout
 
-        textFieldA.setBounds(10, 10, 20, 20);
-        textFieldB.setBounds(50, 10, 20, 20);
-        textFieldC.setBounds(90, 10, 20, 20);
+        textFieldA.setBounds(10, 10, 30, 20);
+        textFieldB.setBounds(80, 10, 30, 20);
+        textFieldC.setBounds(130, 10, 30, 20);
         button.setBounds(10, 50, 100, 30);
         label.setBounds(10, 140, 200, 20);
+        labelSquared.setBounds(40,10,40,20);
+        labelX.setBounds(110,10,40,20);
 
+        panel.add(labelSquared);
+        panel.add(labelX);
         panel.add(textFieldA);
         panel.add(textFieldB);
         panel.add(textFieldC);
@@ -35,17 +39,19 @@ public class GUI implements ActionListener {
         button.addActionListener(this);
 
         frame.setContentPane(panel);
+        frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setBounds(100, 100, 300, 200); // Set frame size and position
         frame.setVisible(true);
     }
     public void actionPerformed(ActionEvent event) {
         if(event.getSource() == button) {
-           textA = textFieldA.getText();
+           String textA = textFieldA.getText();
            String textB = textFieldB.getText();
+           String textC = textFieldC.getText();
            textFieldA.selectAll();
            System.out.println(textA);
-           label.setText("Result: " + textA);
+           label.setText("Result: " + textA + textB + textC);
        }
     }
 }
