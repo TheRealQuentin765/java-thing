3e30cc7df24a1fd95fd12db29972f39cb9367888
rhonegoodfellow <boomcopia8@gmail.com>
2023-05-11 18:28:26 -0700

    GUI is done and routed to the quadratic calculator

--- GUI.java
+++ GUI.java
@@ -4,18 +4,19 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class GUI implements ActionListener {
-    JFormattedTextField textFieldA = new JFormattedTextField("a");
-    JFormattedTextField textFieldB = new JFormattedTextField("b");
-    JFormattedTextField textFieldC = new JFormattedTextField("c");
-
+    JFormattedTextField textFieldA = new JFormattedTextField("");
+    JFormattedTextField textFieldB = new JFormattedTextField("");
+    JFormattedTextField textFieldC = new JFormattedTextField("");
+    JFormattedTextField blankTextField = new JFormattedTextField("");
     JLabel labelSquared = new JLabel("x^2 +");
     JLabel labelX = new JLabel("x +");
 
     JButton button = new JButton("Calculate");
     JPanel panel = new JPanel();
     JFrame frame = new JFrame("Quadratic equationator");
-    JLabel label = new JLabel("Result: ");
-    String textA;
+    JLabel x1 = new JLabel("X = ");
+    JLabel x2 = new JLabel("X = ");
+
 
     public GUI() {
         panel.setLayout(null); // Use null layout
@@ -24,7 +25,8 @@ public class GUI implements ActionListener {
         textFieldB.setBounds(80, 10, 30, 20);
         textFieldC.setBounds(130, 10, 30, 20);
         button.setBounds(10, 50, 100, 30);
-        label.setBounds(10, 140, 200, 20);
+        x1.setBounds(10, 80, 200, 20);
+        x2.setBounds(10, 100, 200, 20);
         labelSquared.setBounds(40,10,40,20);
         labelX.setBounds(110,10,40,20);
 
@@ -34,7 +36,8 @@ public class GUI implements ActionListener {
         panel.add(textFieldB);
         panel.add(textFieldC);
         panel.add(button);
-        panel.add(label);
+        panel.add(x1);
+        panel.add(x2);
 
         button.addActionListener(this);
 
@@ -45,13 +48,27 @@ public class GUI implements ActionListener {
         frame.setVisible(true);
     }
     public void actionPerformed(ActionEvent event) {
+        Double A;
+        Double B;
+        Double C;
         if(event.getSource() == button) {
-           String textA = textFieldA.getText();
-           String textB = textFieldB.getText();
-           String textC = textFieldC.getText();
-           textFieldA.selectAll();
-           System.out.println(textA);
-           label.setText("Result: " + textA + textB + textC);
+            if(textFieldA.equals(blankTextField)){
+                A = 1.0;
+            }else{
+                A = Double.parseDouble(textFieldA.getText());}
+            if(textFieldB == blankTextField){
+                B = 1.0;
+            }else{
+                B = Double.parseDouble(textFieldB.getText());
+            }
+           if(textFieldC == blankTextField){
+               C = 0.0;
+           }else{
+               C = Double.parseDouble(textFieldC.getText());
+           }
+
+           x1.setText("X = " + QuadraticCalculator.calculate(A,B,C).getNum2());
+           x2.setText("X = " + QuadraticCalculator.calculate(A,B,C).getNum1());
        }
     }
 }
