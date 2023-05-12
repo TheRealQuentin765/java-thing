9b97917d6e6ab7230ef44051972d6fec8ab8017d
TheRealQuentin765 <quentin@team-farrell.com>
2023-05-12 08:53:49 -0700

    Added in the abs method in ComplexValue Added toString in Pair and ComplexValue Rewrote Pair to use ComplexValues instead of doubles Rewrote QuadraticCalculator to handle the changes in Pair and handle complex results

--- ComplexValue.java
+++ ComplexValue.java
@@ -38,6 +38,10 @@ public class ComplexValue {
         );
     }
 
+    public ComplexValue multiply(double num) {
+        return new ComplexValue(real*num,imaginary*num);
+    }
+
     public ComplexValue negative() {
         return new ComplexValue(-real, -imaginary);
     }
@@ -45,6 +49,10 @@ public class ComplexValue {
     public ComplexValue inverse() { // the formula comes from the internet
         double d = Math.pow(real,2)+Math.pow(imaginary,2);
         return new ComplexValue(real/d,-imaginary/d);
+    }
+
+    public ComplexValue divide(double num) {
+        return multiply(1/num);
     }
 
     public ComplexValue divide(ComplexValue obj) {
@@ -65,9 +73,11 @@ public class ComplexValue {
 
     public ComplexValue squareRoot() { // this formula also comes from the internet
         double r = radius();
+        int imaginarySign = (int)Math.signum(imaginary);
+        if (imaginarySign == 0) imaginarySign = 1;
         return new ComplexValue(
                 Math.sqrt((r+real)/2),
-                Math.signum(imaginary) * Math.sqrt((r-real)/2)
+                imaginarySign * Math.sqrt((r-real)/2)
         );
     }
 
@@ -86,4 +96,14 @@ public class ComplexValue {
         );
     }
 
+    public ComplexValue abs() {
+        int sign = -(int)Math.signum(real);
+        if (sign == 0) return new ComplexValue(0, Math.abs(imaginary));
+        return new ComplexValue(sign*real,sign*imaginary);
+    }
+
+    @Override
+    public String toString() {
+        return "(" + real + " + " + imaginary + "i)";
+    }
 }
